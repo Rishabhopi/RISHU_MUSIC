c406a650cec7734648dd7de1dfb37225c26488fc
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-07-13 18:58:29 +0530

    Update mustjoin.py

--- SHUKLAMUSIC/plugins/bot/mustjoin.py
+++ SHUKLAMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from SHUKLAMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "Ur_rishu_143"
+MUST_JOIN = "payoutnexu"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
